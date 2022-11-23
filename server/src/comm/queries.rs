c160b2d3694c03143e5604ce6b546f8fbc4fa86a
super-phreak <joshpelino@gmail.com>
2022-11-22 16:09:35 -0800

    Added log to cargo

--- server/src/comm/queries.rs
+++ server/src/comm/queries.rs
@@ -1,6 +1,5 @@
 pub const CREATE_USER_TABLE_SQL: &'static str = r#"CREATE TABLE IF NOT EXISTS "users" (
     "username"	TEXT NOT NULL UNIQUE,
-    "salt"	BLOB NOT NULL,
     "hash"	TEXT NOT NULL,
     PRIMARY KEY("username")
 );
